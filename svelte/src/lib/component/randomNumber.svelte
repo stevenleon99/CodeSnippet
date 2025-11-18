1700106a2b4184d8f31b9e8a0712ae55f36b7886
Steve <1301637976@qq.com>
2025-11-17 23:06:35 -0500

    svelte reactivity demo

---
+++ svelte/src/lib/component/randomNumber.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+    let randNum = $state((Math.random() * 10) + 1);
+    // derive value is synchronouse but the effect function will be pushed to microstack
+    // so that don't use derive value to synchronously update another state value
+    let randNumDerived = $derived.by(() => randNum * 2);
+
+</script>
+
+<h2>Random Number: {randNum}</h2>
+<h2>Random Number Derived (x2): {randNumDerived}</h2>
+<button onclick={() => {
+    randNum = (Math.random() * 10) + 1;
+    console.log('New Random Number Generated:', randNum);
+}}>
+    Generate New Random Number</button>
+
+<style>
+    h2 {
+        color: darkorange;
+    }
+</style>
--- svelte/src/routes/+page.svelte
+++ svelte/src/routes/+page.svelte
@@ -1,2 +1,36 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+    import User from '$lib/component/user.svelte';
+    import RandomNumber from '$lib/component/randomNumber.svelte';
+
+    let firstname = $state('Steve');
+    let lastname = $state('Josh');
+    let fullName = $derived.by(() => `${firstname} ${lastname}`); // derived variable has memory (compared to function), which means it only updates onnce if two elements depend on it and its value doesn't change.
+    let username = $state('xxxxxxxxxx');
+    let src = 'https://cdn.pixabay.com/photo/2017/07/23/21/29/globus-2532875_1280.png';
+    let bio = 'Hello, <b>this</b> is in bold';
+
+    $effect(() => {
+        if (username || firstname ) {
+            console.log('username or firstname changed');
+        }
+    });     
+</script>
+
+<!-- $prop() is able to pass props to components from parent to child. -->
+<User data="hello" />
+<RandomNumber />
+
+
+<input bind:value={username} />
+<input bind:value={firstname} />
+<input bind:value={lastname} />
+
+<h1>{username || fullName}</h1>
+<p>{@html bio}</p>
+
+<style>
+
+    h1{
+        color:cornflowerblue;
+    }
+</style>
